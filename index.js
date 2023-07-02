be350e8f77531a779959067180359710122e6a44
sheikhfoysaldiu <foysal15-3442@diu.edu.bd>
2023-07-02 17:16:41 +0600

    Getting Balance From Wallet

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const {Connection, PublicKey, clusterApiUrl,Keypair,LAMPORTS_PER_SOL} = require(
 
 const wallet = new Keypair()
 
-const publicKey = wallet._keypair.publicKey
+const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
 // console.log("Wallet Public Key: ", publicKey.toString())
 // console.log("Wallet Secret Key: ", secretKey.toString())
 
+const getWalletBalance = async () =>{
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"),'confirmed')
+        const balance = await connection.getBalance(publicKey)
+        console.log(`Wallet Balance: ${balance} `)
+        
+    } catch (error) {
+        
+    }
+}
 
+const main = async () =>{
+    await getWalletBalance()
+}
+
+main()
 
 
 
